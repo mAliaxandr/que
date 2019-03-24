2b14e3246920c5714882822b49da2e9dc82e35de
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2019-03-24 23:31:17 +0300

    all

---
+++ src/queue.js
@@ -0,0 +1,30 @@
+const MaxHeap = require('./max-heap.js');
+
+class PriorityQueue {
+	constructor(maxSize) {
+		this.maxSize = maxSize || 30;
+		this.heap = new MaxHeap;
+	}
+
+	push(data, priority) {
+		if (this.heap.size() === this.maxSize) 
+		throw new Error('PriorityQueue has the maximum number of elements')
+		this.heap.push(data,priority);
+	}
+
+	shift() {
+		if (this.heap.isEmpty()) 
+		throw new Error('PriorityQueue is empty');
+		return this.heap.pop();
+	}
+
+	size() {
+		return this.heap.size();
+	}
+
+	isEmpty() {
+		return this.heap.isEmpty();
+	}
+}
+
+module.exports = PriorityQueue;
